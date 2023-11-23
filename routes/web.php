2bd0968ba7b4cbff3ba3bc39e855dacc1509516a
Vaibhav Singh <singhvaibhav654@gmail.com>
2023-11-23 16:11:59 +0530

    first commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\todosController;
+
+
+Route::get('/', [todosController::class,'index'])->name("todo.home");
+
+Route::get('/create', function () {
+    return view('create');
+})->name("todo.create");
+
+//edit route
+Route::get('/edit/{id}', [todosController::class,'edit'])->name("todo.edit");
+
+//update todo
+Route::post('/update', [todosController::class,'updateData'])->name("todo.updateData");
+
+//create todo route
+Route::post('/create',[todosController::class,'store'])->name("todo.store");
+
+//delete todo route
+Route::get('/delete/{id}',[todosController::class,'delete'])->name("todo.delete");
+
+
